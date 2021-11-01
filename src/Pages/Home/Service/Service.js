51385a315d38f6b78e2488e09d5eabcde46c73ef
Jahid <jahidulhaque767@gmail.com>
2021-11-02 02:39:35 +0600

    added appointment banner and complete service

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,41 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
-const Service = () => {
+const Service = (props) => {
+  const { img, name, description } = props.service;
   return (
-    <div>
-      <h2>This is single service</h2>
-    </div>
+    <>
+      <Grid item xs={4} sm={4} md={4}>
+        <Card
+          sx={{
+            minWidth: 275,
+            border: 0,
+            boxShadow: 0,
+            textAlign: "center",
+            mt: "3rem",
+          }}
+        >
+          <CardMedia
+            component="img"
+            style={{ width: "auto", height: "80px", margin: "0 auto" }}
+            image={img}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {name}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {description}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </>
   );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,30 +1,59 @@
 import React from "react";
-import { experimentalStyled as styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import { Container } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Service from "../Service/Service";
+import fluoride from "../../../images/fluoride.png";
+import cavity from "../../../images/cavity.png";
+import whitening from "../../../images/whitening.png";
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+const services = [
+  {
+    img: fluoride,
+    name: "Fluoride Treatment",
+    description:
+      "Fluoride treatments are typically professional treatments containing a high concentration of fluoride that a dentist or hygienist will apply to a persons teeth to improve health and reduce",
+  },
+  {
+    img: cavity,
+    name: "Cavity Filling",
+    description:
+      "If dental decay is caught in its early stages – before it progresses into the tooth, a filling may not be necessary. In fact, it can take four to eight years for surface decay to penetrate ",
+  },
+  {
+    img: whitening,
+    name: "Teeth Whitening",
+    description:
+      "The quickest, most dramatic way to whiten is to get an in-office bleaching at a dentist's office. Using professional-strength hydrogen peroxide and special lights or lasers, your",
+  },
+];
 
 const Services = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Container>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ textAlign: "center", color: "#18D2B0", mb: "1rem" }}
+        >
+          Our Services
+        </Typography>
+        <Typography
+          variant="h3"
+          component="div"
+          sx={{ textAlign: "center", fontWeight: 600 }}
+        >
+          Services We Provide
+        </Typography>
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          {Array.from(Array(6)).map((_, index) => (
-            <Grid item xs={4} sm={4} md={4} key={index}>
-              <Item>xs=2</Item>
-            </Grid>
+          {services.map((service) => (
+            <Service key={service.name} service={service}></Service>
           ))}
         </Grid>
       </Container>
