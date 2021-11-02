2fd06859199c2f7c1e6a1217b381444fd9e4e9c7
Jahid <jahidulhaque767@gmail.com>
2021-11-03 03:21:57 +0600

    complete booking submitting

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -1,38 +1,51 @@
 import { Button, Grid, Paper, Typography } from "@mui/material";
 import React from "react";
+import BookingModal from "../BookingModal/BookingModal";
 
-const Booking = (props) => {
-  const { name, time, space } = props.booking;
+const Booking = ({ booking, date }) => {
+  const { name, time, space } = booking;
+  const [openBooking, setBookingOpen] = React.useState(false);
+  const handleBookingOpen = () => setBookingOpen(true);
+  const handleBookingClose = () => setBookingOpen(false);
   return (
-    <Grid item xs={12} sm={6} md={4}>
-      <Paper elevation={3} sx={{ py: 2, textAlign: "center" }}>
-        <Typography
-          sx={{ color: "#18d2b0" }}
-          variant="h5"
-          gutterBottom
-          component="div"
-        >
-          {name}
-        </Typography>
-        <Typography variant="h6" gutterBottom component="div">
-          {time}
-        </Typography>
-        <Typography variant="caption" display="block">
-          {space} SPACES AVAILABLE
-        </Typography>
-        <Button
-          variant="contained"
-          sx={{
-            mt: 1,
-            py: 1.3,
-            background:
-              "linear-gradient(to right, rgba(24, 210, 176, 1), rgb(15, 207, 233, 1))",
-          }}
-        >
-          Booking Appointment
-        </Button>
-      </Paper>
-    </Grid>
+    <>
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper elevation={3} sx={{ py: 2, textAlign: "center" }}>
+          <Typography
+            sx={{ color: "#18d2b0" }}
+            variant="h5"
+            gutterBottom
+            component="div"
+          >
+            {name}
+          </Typography>
+          <Typography variant="h6" gutterBottom component="div">
+            {time}
+          </Typography>
+          <Typography variant="caption" display="block">
+            {space} SPACES AVAILABLE
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{
+              mt: 1,
+              py: 1.3,
+              background:
+                "linear-gradient(to right, rgba(24, 210, 176, 1), rgb(15, 207, 233, 1))",
+            }}
+            onClick={handleBookingOpen}
+          >
+            Booking Appointment
+          </Button>
+        </Paper>
+      </Grid>
+      <BookingModal
+        date={date}
+        booking={booking}
+        openBooking={openBooking}
+        handleBookingClose={handleBookingClose}
+      ></BookingModal>
+    </>
   );
 };
 
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,10 +1,79 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 
-const BookingModal = () => {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: 3,
+  p: 4,
+};
+
+const BookingModal = ({ openBooking, handleBookingClose, booking, date }) => {
+  const { name, time } = booking;
+  const handleBookSubmit = (e) => {
+    alert("Booking Submitted");
+    handleBookingClose();
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h2>Booking Modal</h2>
-    </div>
+    <Modal
+      open={openBooking}
+      onClose={handleBookingClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          {name}
+        </Typography>
+        <form>
+          <TextField
+            disabled
+            sx={{ width: "100%", pb: 2 }}
+            id="outlined-size-small"
+            defaultValue={time}
+            size="small"
+          />
+          <TextField
+            sx={{ width: "100%", pb: 2 }}
+            id="outlined-size-small"
+            placeholder="Your Name"
+            size="small"
+          />
+          <TextField
+            sx={{ width: "100%", pb: 2 }}
+            id="outlined-size-small"
+            placeholder="Phone Number"
+            size="small"
+          />
+          <TextField
+            sx={{ width: "100%", pb: 2 }}
+            id="outlined-size-small"
+            placeholder="Email"
+            size="small"
+          />
+          <TextField
+            disabled
+            sx={{ width: "100%", pb: 2 }}
+            id="outlined-size-small"
+            defaultValue={date.toDateString()}
+            size="small"
+          />
+          <Button onClick={handleBookSubmit} variant="contained" type="submit">
+            Send
+          </Button>
+        </form>
+      </Box>
+    </Modal>
   );
 };
 
