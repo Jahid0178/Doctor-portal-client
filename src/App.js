a1ad41aa81154c9c9c79a0805981f0c128e0ea75
Jahid <jahidulhaque767@gmail.com>
2021-11-03 00:06:41 +0600

    added appointment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home/Home";
+import Appointment from "./Pages/Appointment/Appointment";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route path="/home">
-            <Home />
+          <Route exact path="/">
+            <Home></Home>
           </Route>
-          <Route>
-            <Home />
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/appointment">
+            <Appointment></Appointment>
           </Route>
         </Switch>
       </Router>
