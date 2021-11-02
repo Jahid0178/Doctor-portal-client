ce4d4a0c86e7395be9a9b674270ec30df21f85c9
Jahid <jahidulhaque767@gmail.com>
2021-11-03 02:24:36 +0600

    modify navigation

--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -24,7 +24,7 @@ const Navigation = () => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Doctors Portal
+            <Link to="/home">Doctors Portal</Link>
           </Typography>
           <Link to="/appointment">
             <Button color="inherit">Appointment</Button>
