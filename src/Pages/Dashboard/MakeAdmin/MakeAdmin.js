44bfb8449b371382ccb6ecac48a4a0f0e1517778
Jahid <jahidulhaque767@gmail.com>
2021-11-07 04:09:55 +0600

    add admin

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const MakeAdmin = () => {
+  const [email, setEmail] = useState("");
+  const handleOnBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleAdminSubmit = (e) => {
+    e.preventDefault();
+    const user = { email };
+    fetch("http://localhost:4000/users/admin", {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div>
       <h2>Make An Admin</h2>
+      <form onSubmit={handleAdminSubmit}>
+        <TextField
+          label="Email"
+          onBlur={handleOnBlur}
+          type="email"
+          variant="standard"
+        />
+        <Button variant="contained" type="submit">
+          Make Admin
+        </Button>
+      </form>
     </div>
   );
 };
